1dadb79d339ae483264227fdb9f12198d40f60ed
catsanex <tiktokegastwnn@gmail.com>
2025-11-01 03:20:32 +0700

    Update ethers.js

--- ethers.js
+++ ethers.js
@@ -1,10 +1,8 @@
 import { ethers } from "ethers";
 
 const rpc = process.env.RPC_URL || "https://mainnet.base.org";
-
 export const provider = new ethers.JsonRpcProvider(rpc);
 
-// PRIVATE_KEY HARUS terisi. Address ini yang memanggil token.mint()
 if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY.length < 66) {
   console.error("❌ PRIVATE_KEY tidak valid / belum diisi");
   process.exit(1);
